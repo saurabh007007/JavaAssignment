dcc2bc8de6c38de18270add4417273142933ad70
Saurabh Yadav <116506457+saurabh007007@users.noreply.github.com>
2023-10-11 07:30:20 +0530

    Create input_java.java

---
+++ java/input_java.java
@@ -0,0 +1,11 @@
+// Program to take input and print the input
+import java.util.*;
+public class input_java{
+  public static void main(String arg[]){
+    Scanner sc =new Scanner(System.in);
+    int n;
+    System.out.println("Enter your no");
+    n=sc.nextInt();
+    System.out.println("Input number be "+n);
+  }
+}
