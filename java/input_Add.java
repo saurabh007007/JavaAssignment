0f5742cbe2584cc4af64d3aba9c7bcf0147073b6
Saurabh Yadav <116506457+saurabh007007@users.noreply.github.com>
2023-10-12 10:52:40 +0530

    Create input_Add.java

---
+++ java/input_Add.java
@@ -0,0 +1,10 @@
+// Input and add code
+import java.util.*;
+public class input_Add{
+  public static void main(String arg[]){
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println("Sum be ::"+(n+b));
+  }
+}
